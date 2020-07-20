c79e04d78c4471a894c488c266ecefe4c5b4dd7a
Pawan0411 <pawankumar17264@gmail.com>
2020-07-20 15:26:38 +0530

    fix hamburger icon issue, refactored code

--- app/src/main/java/com/adarsh/smartcalculator/MainActivity.java
+++ app/src/main/java/com/adarsh/smartcalculator/MainActivity.java
@@ -1,4 +1,5 @@
 package com.adarsh.smartcalculator;
+
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -21,7 +22,6 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -39,32 +39,35 @@ import java.util.Locale;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    TextView txt,result;
-    ImageButton voice,img;
-    String statement=" ";
-    Button clear,b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,badd,bsub,bmul,bdiv,bpoint,bequal,undo;
+    TextView txt, result;
+    ImageButton voice, img;
+    String statement = " ";
+    Button clear, b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, badd, bsub, bmul, bdiv, bpoint, bequal, undo;
     ImageView imageView;
     Bitmap imageBitmap;
     Switch stxt;
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
         setSupportActionBar(toolbar);
+
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer , toolbar,
-                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(android.R.color.white));
         navigationView.setNavigationItemSelectedListener(this);
 //        if(savedInstanceState == null){
 //            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new homeFragment()).commit();
@@ -72,144 +75,149 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //        }
 
 
-
 //        stxt= findViewById(R.id.switchtxt);
-        txt=findViewById(R.id.msg);
-        voice=findViewById(R.id.voice);
-        img=findViewById(R.id.scan);
-        result=findViewById(R.id.res);
-        clear=findViewById(R.id.clear);
+        txt = findViewById(R.id.msg);
+        voice = findViewById(R.id.voice);
+        img = findViewById(R.id.scan);
+        result = findViewById(R.id.res);
+        clear = findViewById(R.id.clear);
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                statement="";
+                statement = "";
                 txt.setText("");
                 result.setText("");
-                imageView.setVisibility(v.INVISIBLE);
-                b0.setVisibility(v.VISIBLE);
-                b1.setVisibility(v.VISIBLE);
-                b2.setVisibility(v.VISIBLE);
-                b3.setVisibility(v.VISIBLE);
-                b4.setVisibility(v.VISIBLE);
-                b5.setVisibility(v.VISIBLE);
-                b6.setVisibility(v.VISIBLE);
-                b7.setVisibility(v.VISIBLE);
-                b8.setVisibility(v.VISIBLE);
-                b9.setVisibility(v.VISIBLE);
-                badd.setVisibility(v.VISIBLE);
-                bsub.setVisibility(v.VISIBLE);
-                bmul.setVisibility(v.VISIBLE);
-                bdiv.setVisibility(v.VISIBLE);
-                bpoint.setVisibility(v.VISIBLE);
-                bequal.setVisibility(v.VISIBLE);
+                imageView.setVisibility(View.INVISIBLE);
+                b0.setVisibility(View.VISIBLE);
+                b1.setVisibility(View.VISIBLE);
+                b2.setVisibility(View.VISIBLE);
+                b3.setVisibility(View.VISIBLE);
+                b4.setVisibility(View.VISIBLE);
+                b5.setVisibility(View.VISIBLE);
+                b6.setVisibility(View.VISIBLE);
+                b7.setVisibility(View.VISIBLE);
+                b8.setVisibility(View.VISIBLE);
+                b9.setVisibility(View.VISIBLE);
+                badd.setVisibility(View.VISIBLE);
+                bsub.setVisibility(View.VISIBLE);
+                bmul.setVisibility(View.VISIBLE);
+                bdiv.setVisibility(View.VISIBLE);
+                bpoint.setVisibility(View.VISIBLE);
+                bequal.setVisibility(View.VISIBLE);
             }
         });
-        b0=findViewById(R.id.b0);
-        b1=findViewById(R.id.b1);
-        b2=findViewById(R.id.b2);
-        b3=findViewById(R.id.b3);
-        b4=findViewById(R.id.b4);
-        b5=findViewById(R.id.b5);
-        b6=findViewById(R.id.b6);
-        b7=findViewById(R.id.b7);
-        b8=findViewById(R.id.b8);
-        b9=findViewById(R.id.b9);
-        badd=findViewById(R.id.add);
-        bsub=findViewById(R.id.sub);
-        bmul=findViewById(R.id.mul);
-        bdiv=findViewById(R.id.div);
-        bpoint=findViewById(R.id.point);
-        bequal=findViewById(R.id.equal);
-        imageView=findViewById(R.id.imageView);
+        b0 = findViewById(R.id.b0);
+        b1 = findViewById(R.id.b1);
+        b2 = findViewById(R.id.b2);
+        b3 = findViewById(R.id.b3);
+        b4 = findViewById(R.id.b4);
+        b5 = findViewById(R.id.b5);
+        b6 = findViewById(R.id.b6);
+        b7 = findViewById(R.id.b7);
+        b8 = findViewById(R.id.b8);
+        b9 = findViewById(R.id.b9);
+        badd = findViewById(R.id.add);
+        bsub = findViewById(R.id.sub);
+        bmul = findViewById(R.id.mul);
+        bdiv = findViewById(R.id.div);
+        bpoint = findViewById(R.id.point);
+        bequal = findViewById(R.id.equal);
+        imageView = findViewById(R.id.imageView);
         img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
-                imageView.setVisibility(v.VISIBLE);
-                b0.setVisibility(v.INVISIBLE);
-                b1.setVisibility(v.INVISIBLE);
-                b2.setVisibility(v.INVISIBLE);
-                b3.setVisibility(v.INVISIBLE);
-                b4.setVisibility(v.INVISIBLE);
-                b5.setVisibility(v.INVISIBLE);
-                b6.setVisibility(v.INVISIBLE);
-                b7.setVisibility(v.INVISIBLE);
-                b8.setVisibility(v.INVISIBLE);
-                b9.setVisibility(v.INVISIBLE);
-                badd.setVisibility(v.INVISIBLE);
-                bsub.setVisibility(v.INVISIBLE);
-                bmul.setVisibility(v.INVISIBLE);
-                bdiv.setVisibility(v.INVISIBLE);
-                bpoint.setVisibility(v.INVISIBLE);
-                bequal.setVisibility(v.INVISIBLE);
+                imageView.setVisibility(View.VISIBLE);
+                b0.setVisibility(View.INVISIBLE);
+                b1.setVisibility(View.INVISIBLE);
+                b2.setVisibility(View.INVISIBLE);
+                b3.setVisibility(View.INVISIBLE);
+                b4.setVisibility(View.INVISIBLE);
+                b5.setVisibility(View.INVISIBLE);
+                b6.setVisibility(View.INVISIBLE);
+                b7.setVisibility(View.INVISIBLE);
+                b8.setVisibility(View.INVISIBLE);
+                b9.setVisibility(View.INVISIBLE);
+                badd.setVisibility(View.INVISIBLE);
+                bsub.setVisibility(View.INVISIBLE);
+                bmul.setVisibility(View.INVISIBLE);
+                bdiv.setVisibility(View.INVISIBLE);
+                bpoint.setVisibility(View.INVISIBLE);
+                bequal.setVisibility(View.INVISIBLE);
             }
         });
 
-        }
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.home:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new homeFragment()).commit();
                 break;
-            case R.id.instruction :
+            case R.id.instruction:
+            case R.id.textSpeech:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new rules()).commit();
                 break;
-            case R.id.about :
-                break;
-            case R.id.textSpeech :
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new rules()).commit();
-                break;
-            case R.id.dark :
-                break;
-            case R.id.code :
-                break;
-            case R.id.share :
+            case R.id.about:
+            case R.id.dark:
+            case R.id.code:
+            case R.id.share:
                 break;
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
     @Override
     public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START))
+        if (drawer.isDrawerOpen(GravityCompat.START))
             drawer.closeDrawer(GravityCompat.START);
         else
-        super.onBackPressed();
+            super.onBackPressed();
 
     }
 
     public void make_statement(View view) {
-        Button b = (Button)view;
+        Button b = (Button) view;
 
-        statement  =statement + b.getText().toString();
+        statement = statement + b.getText().toString();
         txt.setText(statement);
     }
-    public void evaluate(View view)throws  Exception{
-        Button b = (Button)view;
+
+    public void evaluate(View view) throws Exception {
+        Button b = (Button) view;
         calcspeech();
     }
+
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
-    public void getSpeechText(View view){
-        Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+
+    public void getSpeechText(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        if(intent.resolveActivity(getPackageManager())!=null)
-            startActivityForResult(intent,10);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(intent, 10);
         else
             Toast.makeText(this, "Your Device not Support Speech Input", Toast.LENGTH_SHORT).show();
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap) extras.get("data");
+            Bundle extras = null;
+            if (data != null) {
+                extras = data.getExtras();
+            }
+            if (extras != null) {
+                imageBitmap = (Bitmap) extras.get("data");
+            }
             imageView.setImageBitmap(imageBitmap);
             FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
             FirebaseVision firebaseVision = FirebaseVision.getInstance();
@@ -218,7 +226,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             task.addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
                 @Override
                 public void onSuccess(FirebaseVisionText firebaseVisionText) {
-                    statement=" ";
+                    statement = " ";
                     statement = firebaseVisionText.getText();
                     try {
 //                        txt.setText(statement);
@@ -236,11 +244,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             });
         }
 
-        switch (requestCode){
+        switch (requestCode) {
             case 10:
-                if(resultCode == RESULT_OK && data!= null){
-                    ArrayList<String> res= data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    statement=res.get(0).toString();
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> res = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    statement = res.get(0);
 //                    txt.setText(statement);
                     try {
                         calcspeech();
@@ -250,46 +258,43 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 }
         }
     }
-    public void calcspeech() throws Exception{
-        statement = statement.replaceAll ("plus", "+");
-        statement = statement.replaceAll ("add", "+");
-        statement = statement.replaceAll ("minus", "-");
-        statement = statement.replaceAll ("subtract", "-");
-        statement = statement.replaceAll ("multiply by", "*");
-        statement = statement.replaceAll ("x", "*");
-        statement = statement.replaceAll ("multiplied by", "*");
-        statement = statement.replaceAll ("into", "*");
-        statement = statement.replaceAll ("multiply", "*");
-        statement = statement.replaceAll ("divided by", "/");
-        statement = statement.replaceAll ("divide", "/");
-        statement = statement.replaceAll ("divided", "/");
-        statement = statement.replaceAll ("by", "/");
-        statement = statement.replaceAll ("mod", "%");
-        statement = statement.replaceAll ("remainder", "%");
+
+    public void calcspeech() throws Exception {
+        statement = statement.replaceAll("plus", "+");
+        statement = statement.replaceAll("add", "+");
+        statement = statement.replaceAll("minus", "-");
+        statement = statement.replaceAll("subtract", "-");
+        statement = statement.replaceAll("multiply by", "*");
+        statement = statement.replaceAll("x", "*");
+        statement = statement.replaceAll("multiplied by", "*");
+        statement = statement.replaceAll("into", "*");
+        statement = statement.replaceAll("multiply", "*");
+        statement = statement.replaceAll("divided by", "/");
+        statement = statement.replaceAll("divide", "/");
+        statement = statement.replaceAll("divided", "/");
+        statement = statement.replaceAll("by", "/");
+        statement = statement.replaceAll("mod", "%");
+        statement = statement.replaceAll("remainder", "%");
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
-        try
-        {
+        try {
             txt.setText(statement);
-            String str=engine.eval(statement).toString();
-            if(str.equalsIgnoreCase("Infinity"))
+            String str = engine.eval(statement).toString();
+            if (str.equalsIgnoreCase("Infinity"))
                 result.setText("ERROR !");
             else {
-                try{
+                try {
                     double d = Double.parseDouble(str);
                     DecimalFormat df = new DecimalFormat("#.#####");
                     df.setRoundingMode(RoundingMode.CEILING);
-                    str=df.format(d).toString();
+                    str = df.format(d);
                     result.setText(str);
-                }
-                catch(NumberFormatException e){
+                } catch (NumberFormatException e) {
                     int i = Integer.parseInt(str);
                     result.setText(i);
                 }
             }
 
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Toast.makeText(this, "Cant Evaluate", Toast.LENGTH_SHORT).show();
         }
     }
